9d0145ea4fbf1ddad6fec127eb7b9d50bb1ffe4a
peterchenadded <peterchenadded@gmail.com>
2021-06-13 21:46:57 +1000

    Create java_opts.sh

---
+++ agents/java_opts.sh
@@ -0,0 +1,16 @@
+# OpenTelemetry:
+# https://github.com/open-telemetry/opentelemetry-java-instrumentation
+[ "${OTEL_EXPORTER}" == "jaeger" ] && JAVA_OPTS="${JAVA_OPTS} \
+  -Dotel.exporter=jaeger \
+  -Dotel.jaeger.endpoint=jaeger:14250 \
+  -Dotel.jaeger.service.name=otel-ui \
+  -javaagent:${APP_HOME}/opentelemetry-javaagent-all.jar"
+
+# Splunk observability
+export OTEL_RESOURCE_ATTRIBUTES="service.name=otel-ui,deployment.environment=dev"
+export OTEL_EXPORTER_OTLP_ENDPOINT='http://splunk-otel-collector:4317'
+
+[ "${OTEL_EXPORTER}" == "splunk-otel" ] && JAVA_OPTS="${JAVA_OPTS} \
+  -javaagent:${APP_HOME}/splunk-otel-javaagent.jar -Dsplunk.metrics.enabled=true"
+
+
