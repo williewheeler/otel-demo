81ef2248b550077b6b4ff05224577a458f762846
Peter Chen <peterchenadded@gmail.com>
2021-06-13 14:30:01 +0000

    fixed script

--- agents/java_opts.sh
+++ agents/java_opts.sh
@@ -1,23 +1,28 @@
+#!/bin/bash
+
 # OpenTelemetry:
 # https://github.com/open-telemetry/opentelemetry-java-instrumentation
-[ "${OTEL_EXPORTER}" == "jaeger" ] && JAVA_OPTS="${JAVA_OPTS} \
+if [ "${OTEL_EXPORTER}" == "jaeger" ]; then
+	JAVA_OPTS="${JAVA_OPTS} \
   -Dotel.exporter=jaeger \
   -Dotel.jaeger.endpoint=jaeger:14250 \
   -Dotel.jaeger.service.name=otel-ui \
   -javaagent:${APP_HOME}/opentelemetry-javaagent-all.jar"
 
-# Splunk observability
-export OTEL_RESOURCE_ATTRIBUTES="service.name=otel-ui,deployment.environment=dev"
-export OTEL_EXPORTER_OTLP_ENDPOINT='http://splunk-otel-collector:4317'
+elif [ "${OTEL_EXPORTER}" == "splunk-otel" ]; then
 
-[ "${OTEL_EXPORTER}" == "splunk-otel" ] && JAVA_OPTS="${JAVA_OPTS} \
-  -javaagent:${APP_HOME}/splunk-otel-javaagent.jar -Dsplunk.metrics.enabled=true"
+  # Splunk observability
+  export OTEL_RESOURCE_ATTRIBUTES="service.name=${APP_NAME},deployment.environment=dev"
+  export OTEL_EXPORTER_OTLP_ENDPOINT='http://splunk-otel-collector:4317'
 
-# Newrelic one
-export NEW_RELIC_APP_NAME="${APP_NAME}"
-export NEW_RELIC_LICENSE_KEY="${NEW_RELIC_LICENSE_KEY}"
+  JAVA_OPTS="${JAVA_OPTS} \
+    -javaagent:${APP_HOME}/splunk-otel-javaagent.jar -Dsplunk.metrics.enabled=true"
 
-[ "${OTEL_EXPORTER}" == "newrelic" ] && JAVA_OPTS="${JAVA_OPTS} \
-  -javaagent:${APP_HOME}/newrelic.jar"
+elif [ "${OTEL_EXPORTER}" == "newrelic" ]; then
+  # Newrelic one
+  export NEW_RELIC_APP_NAME="${APP_NAME}"
+  export NEW_RELIC_LICENSE_KEY="${NEW_RELIC_LICENSE_KEY}"
 
-
+  JAVA_OPTS="${JAVA_OPTS} \
+    -javaagent:${APP_HOME}/newrelic.jar"
+fi
