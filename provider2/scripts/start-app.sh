726d3d00fbdd70978251930720c0236bde634aff
peterchenadded <peterchenadded@gmail.com>
2021-06-13 10:22:23 +1000

    Update start-app.sh

--- provider2/scripts/start-app.sh
+++ provider2/scripts/start-app.sh
@@ -7,20 +7,20 @@ set -e
 
 # OpenTelemetry:
 # https://github.com/open-telemetry/opentelemetry-java-instrumentation
-#JAVA_OPTS="${JAVA_OPTS} \
-#  -Xms${JAVA_XMS} \
-#  -Xmx${JAVA_XMX} \
-#  -Dapplication.name=${APP_NAME} \
-#  -Dapplication.home=${APP_HOME} \
-#  -Dotel.exporter=jaeger \
-#  -Dotel.jaeger.endpoint=jaeger:14250 \
-#  -Dotel.jaeger.service.name=otel-provider2 \
-#  -javaagent:${APP_HOME}/opentelemetry-javaagent-all.jar"
+[ "${OTEL_EXPORTER}" == "splunk-otel" ] && JAVA_OPTS="${JAVA_OPTS} \
+  -Xms${JAVA_XMS} \
+  -Xmx${JAVA_XMX} \
+  -Dapplication.name=${APP_NAME} \
+  -Dapplication.home=${APP_HOME} \
+  -Dotel.exporter=jaeger \
+  -Dotel.jaeger.endpoint=jaeger:14250 \
+  -Dotel.jaeger.service.name=otel-provider2 \
+  -javaagent:${APP_HOME}/opentelemetry-javaagent-all.jar"
 
 export OTEL_RESOURCE_ATTRIBUTES="service.name=${APP_NAME},deployment.environment=dev"
 export OTEL_EXPORTER_OTLP_ENDPOINT='http://splunk-otel-collector:4317'
 
-JAVA_OPTS="${JAVA_OPTS} \
+[ "${OTEL_EXPORTER}" == "splunk-otel" ] && JAVA_OPTS="${JAVA_OPTS} \
   -Xms${JAVA_XMS} \
   -Xmx${JAVA_XMX} \
   -Dapplication.name=${APP_NAME} \
